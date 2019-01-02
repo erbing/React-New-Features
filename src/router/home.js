2a30d0657b8a0d75f2ca1babba3aa21f0a0a8c7c
SmallW <447587073@qq.com>
2019-01-02 09:45:35 +0800

    change fu

--- src/router/home.js
+++ src/router/home.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./all.less";
 
 const Home = () => {
-  return (
-    <div>
-      <h2 className="home">This is home page</h2>
-    </div>
-  );
+  return 123;
 };
 
 export default Home;
