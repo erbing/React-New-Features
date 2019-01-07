5b9f5e4884c7f285f7c9de4c70b23bab51428895
SmallW <447587073@qq.com>
2019-01-07 10:09:01 +0800

    change

--- src/router/home.js
+++ src/router/home.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./all.less";
 
-const Home = () => {
+var home = function() {
+  this.name = "zg";
+  this.age = 18;
+};
+
+var h1 = new home();
+console.log(h1, "------ h1 ------");
+
+var newF = fn => {
+  var obj = {};
+  obj.__proto__ = fn.prototype;
+  fn.call(obj);
+
+  return obj;
+};
+
+var h2 = newF(home);
+console.log(h2, "------ h2 ------");
+
+const Homes = () => {
   return 123;
 };
 
-export default Home;
+export default Homes;
